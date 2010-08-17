710ff229238c7f08ed35ac2f36588bc8cc9a8740
none <none>
2010-08-16 23:22:17 -0700

    bare-bones proof-of-concept Spotlight mds (MetaDataServer) importer plug-in for Mac OS

---
+++ crappile_mac_os_spotlight/crappile-mdimporter/GetMetadataForFile.c
@@ -0,0 +1,95 @@
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreServices/CoreServices.h> 
+
+/* -----------------------------------------------------------------------------
+   Step 1
+   Set the UTI types the importer supports
+  
+   Modify the CFBundleDocumentTypes entry in Info.plist to contain
+   an array of Uniform Type Identifiers (UTI) for the LSItemContentTypes 
+   that your importer can handle
+  
+   ----------------------------------------------------------------------------- */
+
+/* -----------------------------------------------------------------------------
+   Step 2 
+   Implement the GetMetadataForFile function
+  
+   Implement the GetMetadataForFile function below to scrape the relevant
+   metadata from your document and return it as a CFDictionary using standard keys
+   (defined in MDItem.h) whenever possible.
+   ----------------------------------------------------------------------------- */
+
+/* -----------------------------------------------------------------------------
+   Step 3 (optional) 
+   If you have defined new attributes, update the schema.xml file
+  
+   Edit the schema.xml file to include the metadata keys that your importer returns.
+   Add them to the <allattrs> and <displayattrs> elements.
+  
+   Add any custom types that your importer requires to the <attributes> element
+  
+   <attribute name="com_mycompany_metadatakey" type="CFString" multivalued="true"/>
+  
+   ----------------------------------------------------------------------------- */
+
+
+
+/* -----------------------------------------------------------------------------
+    Get metadata attributes from file
+   
+   This function's job is to extract useful information your file format supports
+   and return it as a dictionary
+   ----------------------------------------------------------------------------- */
+
+Boolean GetMetadataForFile(void* thisInterface, 
+			   CFMutableDictionaryRef attributes, 
+			   CFStringRef contentTypeUTI,
+			   CFStringRef pathToFile)
+{
+    /* Pull any available metadata from the file at the specified path */
+    /* Return the attribute keys and attribute values in the dict */
+    /* Return TRUE if successful, FALSE if there was no data provided */
+	
+	const char *bytes;
+
+	bytes = CFStringGetCStringPtr(pathToFile, kCFStringEncodingMacRoman);
+	
+	FILE *fp;
+	fp=fopen(bytes, "r");
+	
+	/**
+	 FIXME / TODO - this needs to be a while loop, _NOT_ a silly "if"
+	 */
+	if ( fp )
+	{
+		static const int BUFFER_SIZE = 500;
+		char buffer[BUFFER_SIZE];
+		
+		while (!feof(fp))
+		{
+			fgets(buffer,BUFFER_SIZE,fp);
+			
+			CFStringRef tagValue = CFStringCreateWithBytes (
+												 kCFAllocatorDefault
+												 , (unsigned char*) buffer 
+												 , strlen(buffer)
+												 , kCFStringEncodingASCII
+												 , false //Boolean isExternalRepresentation
+			);
+			
+			CFDictionarySetValue(attributes, CFSTR("kMDItemFinderComment"), tagValue);
+			CFDictionarySetValue(attributes, CFSTR("kMDItemTextContent"), tagValue);			
+			
+			CFRelease( tagValue );
+		}	
+		
+		fclose(fp);
+		return TRUE;
+	}
+	else
+	{
+		fclose(fp);
+		return FALSE;
+	}
+}
